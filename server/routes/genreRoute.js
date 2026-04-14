2eb8eb882caf547af04ce2d4902d64a440b01aa0
Anastasiia <anastasia.grinke2005@gmail.com>
2026-04-14 15:48:54 +0300

    feat(server): add API routes for all resources

---
+++ server/routes/genreRoute.js
@@ -0,0 +1,10 @@
+const {Router} = require('express')
+const router = new Router()
+const genreController = require('../controllers/genreController')
+
+
+router.post('/', genreController.create)
+
+router.get('/', genreController.getAll)
+
+module.exports = router
